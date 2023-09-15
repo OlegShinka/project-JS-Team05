70131f9630a51aa49f22df7ac884e875b550633b
AntoninaYatsenko <dp130688jaj@gmail.com>
2023-09-15 16:39:50 +0100

    add

---
+++ src/js/categories.js
@@ -0,0 +1,93 @@
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+const allСategorisBtn = document.querySelector('.all-categoris');
+const categorisList = document.querySelector('.categoris-list');
+let activButton = null;
+
+//////запити на бек
+
+async function allCategoris() {
+  const url = `${BASE_URL}/categories`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function oneCategory(cat) {
+  const url = `${BASE_URL}/recipes?category=${cat}`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function allRecipes() {
+  const url = `${BASE_URL}/recipes`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+///////отримання даних
+
+async function getAllCategoris() {
+  activButton = allСategorisBtn;
+  activButton.classList.add('activ');
+  try {
+    const data = await allCategoris();
+    console.log(data);
+    const markup = data
+      .map(
+        cat =>
+          `<button class='categoris-btn' type='button'>${cat.name}</button>`
+      )
+      .join('');
+    categorisList.innerHTML = markup;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getAllCategoris();
+categorisList.addEventListener('click', onCategoryClick);
+async function onCategoryClick(event) {
+  if (event.target.nodeName !== 'BUTTON') return;
+  const category = event.target.textContent;
+  activButton.classList.remove('activ');
+  activButton = event.target;
+  activButton.classList.add('activ');
+  try {
+    const data = await oneCategory(category);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+allСategorisBtn.addEventListener('click', onAllCategorisClick);
+async function onAllCategorisClick() {
+  activButton.classList.remove('activ');
+  activButton = allСategorisBtn;
+  activButton.classList.add('activ');
+  try {
+    const data = await allRecipes();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '../node_modules/modern-normalize/modern-normalize.css';
 import './js/header.js';
 import './js/hero.js';
 import './js/categories.js';
@@ -5,7 +6,3 @@ import './js/popular-rec.js';
 import './js/recipes.js';
 import './js/modal-rec.js';
 import './js/favorites.js';
-
-import '../node_modules/modern-normalize/modern-normalize.css';
-import "/js/categoris";
-
