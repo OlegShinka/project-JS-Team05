f6af9218c1e840e481b1b786198900f3ed03559a
OlegShinka <volkonxxl@gmail.com>
2023-09-15 16:26:16 +0300

    files *.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/categories.js';
+import './js/popular-rec.js';
+import './js/recipes.js';
+import './js/modal-rec.js';
+import './js/favorites.js';
+
 import '../node_modules/modern-normalize/modern-normalize.css';
