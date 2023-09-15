d0aa7eef759a51ae9ad84a299130e809835c3a95
OleksandrFilin <acfiln@ukr.net>
2023-09-16 00:51:59 +0300

    fix h1 heder

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,4 +1,28 @@
 // import Swiper JS
 import Swiper from 'swiper';
-// import Swiper styles
-//import 'swiper/css';
+// // import Swiper styles
+// import 'swiper/css';
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   spaceBetween: 10,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   // Navigation arrows
+//   //   navigation: {
+//   //     nextEl: '.swiper-button-next',
+//   //     prevEl: '.swiper-button-prev',
+//   //   },
+
+//   //   // And if we need scrollbar
+//   //   scrollbar: {
+//   //     el: '.swiper-scrollbar',
+//   // },
+// });
