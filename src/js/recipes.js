e23f26a512ee4122fc347fdb02840a4072a58312
Anton_Lisoval <lisowal@gmail.com>
2023-09-16 08:21:59 +0300

    filter fix

--- src/js/recipes.js
+++ src/js/recipes.js
@@ -1,19 +1,31 @@
 import customSelect from 'custom-select';
 import 'custom-select/src/css/custom-select.css';
 
-const element = {
+const elements = {
+  form: document.querySelector('.filter-form'),
   search: document.querySelector('.search-input'),
   clearSearch: document.querySelector('.clean-search'),
   clearForm: document.querySelector('.reset-filter'),
+  cstSel: customSelect('.custom-select'),
 };
 
-element.clearSearch.addEventListener('click', function () {
-  element.search.value = '';
+elements.clearSearch.addEventListener('click', function () {
+  elements.search.value = '';
 });
 
-element.clearForm.addEventListener('click', function () {
-  element.search.value = '';
-  cstSel.forEach(sel => (sel.value = '0'));
+elements.clearForm.addEventListener('click', function () {
+  elements.search.value = '';
+  elements.cstSel.forEach(sel => (sel.value = '0'));
 });
 
-const cstSel = customSelect('.custom-select');
+elements.search.addEventListener('change', () => console.log(elements.search));
+elements.cstSel.forEach(sel =>
+  sel.select.addEventListener('change', () => {
+    // console.dir(sel);
+    if (sel.value === '0') {
+      sel.opener.style.color = 'rgba(5, 5, 5, 0.5)';
+    } else {
+      sel.opener.style.color = '#050505';
+    }
+  })
+);
