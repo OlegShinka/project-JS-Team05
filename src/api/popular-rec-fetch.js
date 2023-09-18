cdaa153bc46bfb06a92c8e703d0430da941ca2da
ravkamaria <hevkomaria1@gmail.com>
2023-09-18 13:04:36 +0300

    added basicLightBox and modal

--- src/api/popular-rec-fetch.js
+++ src/api/popular-rec-fetch.js
@@ -1,10 +1,24 @@
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
-const END_POINT = "/recipes/popular";
+const END_POINT = '/recipes/';
 
-async function getPopularRecipes () {
-    const response = await fetch(`${BASE_URL}${END_POINT}`);
-    const data = await response.json();
-    return data;
+//пошук популярних рецептів
+async function getPopularRecipes() {
+  const response = await fetch(`${BASE_URL}${END_POINT}popular`);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  const data = await response.json();
+  return data;
 }
 
-export {getPopularRecipes};
+//пошук одного рецепту по ID
+async function getOnePopularRecipe(id) {
+  const response = await fetch(`${BASE_URL}${END_POINT}${id}`);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export { getPopularRecipes, getOnePopularRecipe };
--- src/js/popular-rec.js
+++ src/js/popular-rec.js
@@ -1,19 +1,33 @@
-import { getPopularRecipes } from "../api/popular-rec-fetch";
+import * as basicLightbox from 'basiclightbox';
+// import '/node_modules/basiclightbox/dist/basicLightbox.min.css';
+import {
+  getPopularRecipes,
+  getOnePopularRecipe,
+} from '../api/popular-rec-fetch';
+import { markupRecipeModal } from './templates/recipe-modal-markup';
 
-const popularItems = document.querySelector(".popular-items");
+const popularItems = document.querySelector('.popular-items');
 
 getPopularRecipes()
-    .then(data => {createMarkUp(data);
-      const popularRecItem = document.querySelectorAll(".popular-item");
-        for (const item of popularRecItem) {
-          // console.log("item", item)
-          item.addEventListener("click", showRecipeDitails);
-        }
-    })
-    .catch(error => console.log(error.message));
+  .then(data => {
+    createMarkUp(data);
+    const popularRecItem = document.querySelectorAll('.popular-item');
+    for (const item of popularRecItem) {
+      item.addEventListener('click', showRecipeDitails);
+    }
+  })
+  .catch(error => console.log(error.message));
 
+//відмальовка списку популярних рецептів
 function createMarkUp(arr) {
-    const markup = arr.map(({description, title, preview, _id}) => `<div id=${_id} class="popular-item">
+  const markup = arr
+    .map(
+      ({
+        description,
+        title,
+        preview,
+        _id,
+      }) => `<div id=${_id} class="popular-item">
     <img
       class="popular-item-img"
       src="${preview}"
@@ -23,10 +37,26 @@ function createMarkUp(arr) {
       <h3 class="popular-item-title">${title}</h3>
       <p  class="popular-item-descr">${description}</p>
     </div>
-  </div>`).join("");
+  </div>`
+    )
+    .join('');
   popularItems.insertAdjacentHTML('beforeend', markup);
-  }
-  
-function showRecipeDitails(evt){
-    console.log(evt.currentTarget.id)
 }
+
+//по запиту на бекенд по ID викликає модальне вікно з детальною інфо
+// function showRecipeDitails(evt) {
+//   getOnePopularRecipe(evt.currentTarget.id)
+//     .then(data => {
+//       // console.log(data);
+//       const cardRec = markupRecipeModal(data);
+//       // console.log(cardRec);
+//       const instance = basicLightbox.create(cardRec);
+//       instance.show();
+//       document.addEventListener("keydown", (event) => {
+//         if (event.code === "Escape") {
+//           instance.close();
+//         }
+//       });
+//     })
+//     .catch(error => console.log(error.message));
+// }
