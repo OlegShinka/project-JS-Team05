c98262fee02fbb70612a09f70986a424f670a250
Anton_Lisoval <lisowal@gmail.com>
2023-09-19 13:32:42 +0300

    favorietes funcfional

--- src/main-favorites.js
+++ src/main-favorites.js
@@ -1,9 +1,6 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './js/header.js';
-import './js/hero.js';
-import './js/categories.js';
-import './js/popular-rec.js';
-import './js/recipes.js';
+// import './js/categories.js';
 import './js/modal-rec.js';
-// import './js/favorites.js';
+import './js/favorites.js';
 import './js/pagination.js';
