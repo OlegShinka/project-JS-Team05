dedfee001fe2884dfb45367f2d6f6c661a0e2176
OleksandrFilin <acfiln@ukr.net>
2023-09-21 15:30:15 +0300

    in/of thema

--- src/js/switchdark.js
+++ src/js/switchdark.js
@@ -1,26 +1,32 @@
 // const switch = document.querySelector('.sw-chbox');
 const switchhDark = document.querySelector('.sw-chbox');
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
 const swRefs = {
   switchOn: document.querySelector('.icon-onswitch-mob'),
   switchOff: document.querySelector('.icon-switch-mob'),
 };
 
 const switchhDarkHeder = document.querySelector('.sw-chbox-heder');
-const switchOnHeder = document.querySelector('.icon-onswitch');
-const switchOffHeder = document.querySelector('.icon-switch');
 const swRefsHed = {
   switchOn: document.querySelector('.icon-onswitch'),
   switchOff: document.querySelector('.icon-switch'),
 };
-
-console.log(switchhDark);
-console.dir(switchhDark);
-
+function setThema() {
+  console.log(localStorage.getItem('thema'));
+  if (localStorage.getItem('thema') === 'dark') {
+    //якщо тема в локалі записана темна
+    // bodyEl.classList.add('dark');
+    // switchhDark.checked = true;
+  } else {
+    //  if (bodyEl.classList.contains('dark')){}
+  }
+}
+setThema();
 switchhDark.addEventListener('change', onClickSwitch);
 switchhDarkHeder.addEventListener('change', onClickSwitchHeder);
 function changeSwitch(switchhD, swRf) {
   if (switchhD.checked) {
-    console.log(true);
     swRf.switchOn.classList.remove('is-hidden');
     swRf.switchOff.classList.add('is-hidden');
   } else {
@@ -28,9 +34,20 @@ function changeSwitch(switchhD, swRf) {
     swRf.switchOff.classList.remove('is-hidden');
   }
 }
+function SetTemsLocStorage() {
+  if (bodyEl.classList.contains('dark')) {
+    localStorage.setItem('thema', 'dark');
+  } else {
+    localStorage.setItem('thema', 'light');
+  }
+}
 function onClickSwitch() {
   changeSwitch(switchhDark, swRefs);
+  bodyEl.classList.toggle('dark');
+  SetTemsLocStorage();
 }
 function onClickSwitchHeder() {
   changeSwitch(switchhDarkHeder, swRefsHed);
+  bodyEl.classList.toggle('dark');
+  SetTemsLocStorage();
 }
