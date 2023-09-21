4313b475afab6289b4908b8b63e347c1f0685048
OleksandrFilin <acfiln@ukr.net>
2023-09-21 13:22:38 +0300

    in/of svitch markup on heder

--- src/js/switchdark.js
+++ src/js/switchdark.js
@@ -1,19 +1,36 @@
 // const switch = document.querySelector('.sw-chbox');
 const switchhDark = document.querySelector('.sw-chbox');
-const switchOn = document.querySelector('.icon-onswitch-mob');
-const switchOff = document.querySelector('.icon-switch-mob');
+const swRefs = {
+  switchOn: document.querySelector('.icon-onswitch-mob'),
+  switchOff: document.querySelector('.icon-switch-mob'),
+};
+
+const switchhDarkHeder = document.querySelector('.sw-chbox-heder');
+const switchOnHeder = document.querySelector('.icon-onswitch');
+const switchOffHeder = document.querySelector('.icon-switch');
+const swRefsHed = {
+  switchOn: document.querySelector('.icon-onswitch'),
+  switchOff: document.querySelector('.icon-switch'),
+};
+
 console.log(switchhDark);
 console.dir(switchhDark);
 
 switchhDark.addEventListener('change', onClickSwitch);
-
-function onClickSwitch() {
-  if (switchhDark.checked) {
+switchhDarkHeder.addEventListener('change', onClickSwitchHeder);
+function changeSwitch(switchhD, swRf) {
+  if (switchhD.checked) {
     console.log(true);
-    switchOn.classList.remove('is-hidden');
-    switchOff.classList.add('is-hidden');
+    swRf.switchOn.classList.remove('is-hidden');
+    swRf.switchOff.classList.add('is-hidden');
   } else {
-    switchOn.classList.add('is-hidden');
-    switchOff.classList.remove('is-hidden');
+    swRf.switchOn.classList.add('is-hidden');
+    swRf.switchOff.classList.remove('is-hidden');
   }
+}
+function onClickSwitch() {
+  changeSwitch(switchhDark, swRefs);
+}
+function onClickSwitchHeder() {
+  changeSwitch(switchhDarkHeder, swRefsHed);
 }
